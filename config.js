8b9ddc8205a0c3ec43017b892b2e473d8c5e077a
KhoaXinhGai <86790909+KhoaXinhGai@users.noreply.github.com>
2023-08-17 20:00:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Btuyen ưi!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao Btuyeb cứ đi nhầm đường
+    Lạc vào tim Khoa lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'Phải chăng Btuyen đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng Btuyen đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích Khoa <33',
+    btnNo: 'Không, Khoa mơ à :3',
+    question:'Trên cái đời lày có rất nhiều thằng đẹp trai sao Btuyen lại chọn Khoa dợ',
+    btnReply: 'Gửi cho Khoa <3',
+    reply: 'Bởi vì Khoa là người đẹp trai nhất trong tất cả những người đẹp trai 😘',
+    mess: 'Khoa biết mà 🥰. Yêu Btuyen nhiều lắm 😘😘',
+    messDesc: 'Tối nay tắm rửa thơm tho nằm nghiên mình cho ráo nước đi Khoa qua chơi ha .',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+   
 }
