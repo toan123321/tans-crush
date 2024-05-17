5480f18c138eb629957eaec22b45c6415b05fd7d
toan123321 <98961504+toan123321@users.noreply.github.com>
2024-05-17 23:02:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Btuyen ưi!',
+    introTitle: 'Bvyyy ưi!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao Btuyen cứ đi nhầm đường
-    Lạc vào tim Khoa lẻ loi
+    Mà sao vyyyy cứ đi nhầm đường
+    Lạc vào tim toànnn lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng Btuyen đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng Btuyen đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích Khoa <33',
-    btnNo: 'Không, Khoa mơ à :3',
-    question:'Trên cái thế giới lày có rất nhiều thằng đẹp trai mà sao Btuyen vẫn chọn Khoa vậy ?',
-    btnReply: 'Gửi cho Khoa <3',
-    reply: 'Bởi vì Khoa là đẹp trai nhất trong tất cả những thằng đẹp trai <33333333',
-    mess: 'Khoa biết mà 🥰. Yêu Btuyen nhiều lắm 😘😘',
-    messDesc: 'Tối nay Btuyen tắm rửa thom thơ nằm nghiên mình cho ráo nước đi rồi tối Khoa qua chơi  🤤.',
+    title: 'Phải chăng anhhh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng anhhhhh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích toànnnnnn <33',
+    btnNo: 'Không, toànnn ảo  à :3',
+    question:'Trên cái thế giới lày có rất nhiều thằng đẹp trai mà sao emmmm  vẫn chọn anhhhh vậy ?',
+    btnReply: 'Gửi cho tònnnnnn <3',
+    reply: 'Bởi vì anhhhhhh là đẹp trai nhất trong tất cả những thằng đẹp trai <33333333',
+    mess: 'anhhhh biết mà 🥰. Yêu bé vyyyyyy nhiều lắm 😘😘',
+    messDesc: 'Tối nay béeeeeee tắm rửa thom thơ nằm nghiên mình cho ráo nước đi rồi tối anhhhhhhh qua chơi  🤤.',
     btnAccept: 'Okiiiii lun <3',
 
 }
